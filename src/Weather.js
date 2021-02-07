67c1e02c22e38c3c96b441123db7e065df07ff37
mayazirne <73857372+mayazirne@users.noreply.github.com>
2021-02-07 16:42:14 +0000

    reduced number of js files

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Search from "./Search";
-import ReactAnimatedWeather from 'react-animated-weather';
+import ReactAnimatedWeather from "react-animated-weather";
 
 import "./Weather.css";
 
@@ -17,7 +16,25 @@ export default function Weather() {
   return (
     <div>
       <div className="container-main">
-        <Search />
+        <div className="row">
+      <div className="col-sm">
+        <form className="search-city">
+          <input
+            type="text"
+            id="city-input"
+            className="form"
+            placeholder="Search for a city"
+            autoComplete="off"
+          />
+          <button type="submit">
+            <i className="fas fa-search"></i>
+          </button>
+          <button id="current-location-button">
+            <i className="fas fa-map-marker-alt"></i>
+          </button>
+        </form>
+      </div>
+    </div>
         <h3>{weatherData.time}</h3>
         <div className="wrapper">
           <div className="neon-wrapper">
